3507e2d066f44638759c9ad531fd8e016f743983
ma <qubit.ma@gmail.com>
2021-07-15 09:18:23 +0800

    易签host

--- library/esign/init.go
+++ library/esign/init.go
@@ -1,9 +1,11 @@
 package esign
 
-import "github.com/gogf/gf/frame/g"
+import (
+	"github.com/gogf/gf/frame/g"
+)
 
 func init() {
-	if conf == nil {
+	if conf.host == "" {
 		conf.SetHost(g.Cfg().GetString("eSign.host"))
 		conf.SetProjectId(g.Cfg().GetString("eSign.appId"))
 		conf.SetProjectSecret(g.Cfg().GetString("eSign.appSecret"))
