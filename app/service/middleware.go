3f81fe918afc447da63fbb219a7f851984087738
liasica <magicrolan@qq.com>
2021-07-18 18:32:26 +0800

    全局拦截错误输出

--- app/service/middleware.go
+++ app/service/middleware.go
@@ -10,7 +10,9 @@
 package service
 
 import (
+    "battery/library/response"
     "github.com/gogf/gf/net/ghttp"
+    "log"
     "net/http"
 )
 
@@ -27,10 +29,12 @@ func (s *middlewareService) CORS(r *ghttp.Request) {
 // ErrorHandle 全局错误处理
 func (s *middlewareService) ErrorHandle(r *ghttp.Request) {
     r.Middleware.Next()
+    log.Println("服务器故障啦", r.GetError())
     // 服务器故障抛出错误覆盖
     if r.Response.Status >= http.StatusInternalServerError {
+        r.Response.Status = http.StatusOK
         r.Response.ClearBuffer()
-        r.Response.Writeln("服务器故障了，请稍后重试或联系管理员！")
+        // r.Response.Writeln("服务器故障了，请稍后重试或联系管理员！")
+        response.Json(r, response.RespCodeSystemError, "服务器故障了，请稍后重试或联系管理员！")
     }
 }
-
