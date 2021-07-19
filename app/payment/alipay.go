a00b82c572b9e696053e1b83b279cc328067d4a3
ma <qubit.ma@gmail.com>
2021-07-19 18:05:21 +0800

    支付宝回调日志

--- app/payment/alipay.go
+++ app/payment/alipay.go
@@ -3,6 +3,7 @@ package payment
 import (
 	"battery/app/model"
 	"battery/library/payment/alipay"
+	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 	"github.com/gogf/gf/os/gtime"
 	alipayV3 "github.com/smartwalle/alipay/v3"
@@ -18,10 +19,10 @@ type alipayApi struct {
 func (*alipayApi) PackageOrderNewSuccessCallback(r *ghttp.Request) {
 	res, err := alipay.Service().GetTradeNotification(r.Context(), r.Request)
 	if err != nil || res == nil {
+		g.Log().Error(err)
 		r.Response.Status = http.StatusBadRequest
 		r.Response.Write("error")
 		r.Exit()
-		return
 	}
 	if res.TradeStatus == alipayV3.TradeStatusSuccess {
 		//TODO 查询校验
@@ -40,10 +41,10 @@ func (*alipayApi) PackageOrderNewSuccessCallback(r *ghttp.Request) {
 func (*alipayApi) PackageOrderRenewalSuccessCallback(r *ghttp.Request) {
 	res, err := alipay.Service().GetTradeNotification(r.Context(), r.Request)
 	if err != nil || res == nil {
+		g.Log().Error(err)
 		r.Response.Status = http.StatusBadRequest
 		r.Response.Write("error")
 		r.Exit()
-		return
 	}
 	if res.TradeStatus == alipayV3.TradeStatusSuccess {
 		//TODO 查询校验
@@ -62,6 +63,7 @@ func (*alipayApi) PackageOrderRenewalSuccessCallback(r *ghttp.Request) {
 func (*alipayApi) PackageOrderPenaltySuccessCallback(r *ghttp.Request) {
 	res, err := alipay.Service().GetTradeNotification(r.Context(), r.Request)
 	if err != nil || res == nil {
+		g.Log().Error(err)
 		r.Response.Status = http.StatusBadRequest
 		r.Response.Write("error")
 		r.Exit()
