131eb421ba7da59743bbd63f7c78ed9835203e55
hongbingma <qubit.ma@gmail.com>
2021-08-25 11:00:28 +0800

    sapi/exception 接口报错

--- app/model/exception.go
+++ app/model/exception.go
@@ -16,5 +16,5 @@ type ExceptionReportReq struct {
 	RecoverType uint     `validate:"required" v:"required" json:"recoverType"`     // 1 用户 2 店长
 	Detail      string   `validate:"required" v:"required" json:"detail"`          // 详细说明
 	Img         []string `validate:"required" v:"required" json:"img"`             // 图片链接数组
-	Reason      int      `validate:"required" v:"required|in=0,1,2" json:"reason"` // 故障原因 0 其它 1 插头故障 2 无电压
+	Reason      int      `validate:"required" v:"required|in:0,1,2" json:"reason"` // 故障原因 0 其它 1 插头故障 2 无电压
 }
